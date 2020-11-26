df88a7c12c8760f866080ba802627a3e68f9d01f
git-leon <xleonhunter@gmail.com>
2020-11-26 10:45:18 -0500

    full prototype

--- my-first-query/src/main/java/com/github/curriculeon/config/UserConfig.java
+++ my-first-query/src/main/java/com/github/curriculeon/config/UserConfig.java
@@ -5,6 +5,7 @@ import com.github.curriculeon.models.builders.UserBuilder;
 import com.github.curriculeon.models.builders.UserProfileBuilder;
 import com.github.curriculeon.models.builders.UserWalletBuilder;
 import com.github.curriculeon.services.UserService;
+import com.github.git_leon.RandomUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,44 +29,23 @@ public class UserConfig {
     @PostConstruct
     public void setup() {
         for (int i = 0; i < 9999; i++) {
+            int firstNameLength = RandomUtils.createInteger(5, 10);
+            int lastNameLength = RandomUtils.createInteger(5, 10);
+            String firstName = RandomUtils.createString('A','Z', firstNameLength);
+            String lastName = RandomUtils.createString('A','Z', lastNameLength);
+            Date creationDate = RandomUtils.createDate(2019, 2021);
+            Double walletBalance = RandomUtils.createDouble(10000.0, 9999999.0);
 
+            createUser(new UserBuilder()
+                    .setProfile(new UserProfileBuilder()
+                            .setFirstName(firstName)
+                            .setLastName(lastName)
+                            .setDateCreated(creationDate)
+                            .createUserProfile())
+                    .setWallet(new UserWalletBuilder()
+                            .setBalance(walletBalance)
+                            .createUserWallet())
+                    .createUser());
         }
-    }
-
-    private void create3Users() {
-        Arrays.asList(
-                new UserBuilder()
-                        .setProfile(new UserProfileBuilder()
-                                .setFirstName("Leon")
-                                .setLastName("Hunter")
-                                .setDateCreated(new Date())
-                                .createUserProfile())
-                        .setWallet(new UserWalletBuilder()
-                                .setBalance(999999.99)
-                                .createUserWallet())
-                        .createUser(),
-
-                new UserBuilder()
-                        .setProfile(new UserProfileBuilder()
-                                .setFirstName("Noel")
-                                .setLastName("Retnuh")
-                                .setDateCreated(new Date())
-                                .createUserProfile())
-                        .setWallet(new UserWalletBuilder()
-                                .setBalance(9999998.99)
-                                .createUserWallet())
-                        .createUser(),
-
-                new UserBuilder()
-                        .setProfile(new UserProfileBuilder()
-                                .setFirstName("Leon")
-                                .setLastName("Christopher")
-                                .setDateCreated(new Date())
-                                .createUserProfile())
-                        .setWallet(new UserWalletBuilder()
-                                .setBalance(99999987.99)
-                                .createUserWallet())
-                        .createUser())
-                .forEach(this::createUser);
     }
 }
