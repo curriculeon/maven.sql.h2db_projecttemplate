2857e53bf2824648a98381333ee80a6f0a433e87
git-leon <xleonhunter@gmail.com>
2020-11-26 09:25:57 -0500

    simplified user class

--- my-first-query/src/main/java/com/github/curriculeon/models/User.java
+++ my-first-query/src/main/java/com/github/curriculeon/models/User.java
@@ -8,7 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
 public class User implements EntityInterface<Long> {
@@ -16,14 +15,9 @@ public class User implements EntityInterface<Long> {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String name;
-
-    public User() {
-    }
-
-    public User(Long id, String name) {
-        this.id = id;
-        this.name = name;
+    @Override
+    public String toString() {
+        return Jsonifier.jsonify(this);
     }
 
     @Override
@@ -31,34 +25,8 @@ public class User implements EntityInterface<Long> {
         return id;
     }
 
+    @Override
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(name, user.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return Jsonifier.jsonify(this);
     }
 }
