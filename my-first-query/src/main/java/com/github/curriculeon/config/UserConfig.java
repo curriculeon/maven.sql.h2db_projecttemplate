75faeb305e3fc3d61af826bcff5b080d51f79581
git-leon <xleonhunter@gmail.com>
2020-11-26 09:58:27 -0500

    finished user config;

--- my-first-query/src/main/java/com/github/curriculeon/config/UserConfig.java
+++ my-first-query/src/main/java/com/github/curriculeon/config/UserConfig.java
@@ -1,10 +1,14 @@
 package com.github.curriculeon.config;
 
-import com.github.curriculeon.models.User;
+import com.github.curriculeon.models.builders.UserBuilder;
+import com.github.curriculeon.models.builders.UserProfileBuilder;
+import com.github.curriculeon.models.builders.UserWalletBuilder;
 import com.github.curriculeon.services.UserService;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Date;
 
 @Configuration
 public class UserConfig {
@@ -12,5 +16,39 @@ public class UserConfig {
 
     @PostConstruct
     public void setup() {
+        Arrays.asList(
+                new UserBuilder()
+                        .setProfile(new UserProfileBuilder()
+                                .setFirstName("Leon")
+                                .setLastName("Hunter")
+                                .setDateCreated(new Date())
+                                .createUserProfile())
+                        .setWallet(new UserWalletBuilder()
+                                .setBalance(999999.99)
+                                .createUserWallet())
+                        .createUser(),
+
+                new UserBuilder()
+                        .setProfile(new UserProfileBuilder()
+                                .setFirstName("Noel")
+                                .setLastName("Retnuh")
+                                .setDateCreated(new Date())
+                                .createUserProfile())
+                        .setWallet(new UserWalletBuilder()
+                                .setBalance(9999998.99)
+                                .createUserWallet())
+                        .createUser(),
+
+                new UserBuilder()
+                        .setProfile(new UserProfileBuilder()
+                                .setFirstName("Leon")
+                                .setLastName("Christopher")
+                                .setDateCreated(new Date())
+                                .createUserProfile())
+                        .setWallet(new UserWalletBuilder()
+                                .setBalance(99999987.99)
+                                .createUserWallet())
+                        .createUser())
+                .forEach(userService::create);
     }
 }
